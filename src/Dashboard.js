9d89b6b68ef9765a671e818c3862212f52ed6d2c
igoryen <igor.yentaltsev@gmail.com>
2020-09-24 17:43:19 -0400

    Added router, started Dashboard

---
+++ src/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Dashboard = (props) => (
+    <div>
+        hello from Dashboard
+        <input type="text" placeholder="search texts"/>    
+    </div>
+)
+
+export default Dashboard
