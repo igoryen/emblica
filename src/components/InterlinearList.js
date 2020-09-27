9c8a4392998d7a0b0a4cb1fccd63940f9335e6d5
igoryen <igor.yentaltsev@gmail.com>
2020-09-27 14:41:22 -0400

    Display filters

--- src/components/InterlinearList.js
+++ src/components/InterlinearList.js
@@ -4,13 +4,14 @@ import { connect } from 'react-redux'
 const InterlinearList = (props) => (
     <div>
         <h1>List of Interlinears</h1>
-        {props.interlinears.length}
+        {props.filters.text} | {props.interlinears.length}
     </div>
 )
 
 const mapStateToProps = (state) => {
     return {
-        interlinears: state.interlinears
+        interlinears: state.interlinears,
+        filters: state.filters
     }
 }
 
