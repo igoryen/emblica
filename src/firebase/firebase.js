11d3f782bf35f37f8d4353947629aef4c91f5dfe
igoryen <igor.yentaltsev@gmail.com>
2020-10-04 22:30:59 -0400

    Reordered env vars

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,16 @@
 import * as firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
+    appId: process.env.REACT_APP_APP_ID,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
+console.log("process.env.REACT_APP_PROJECT_ID:", process.env.REACT_APP_PROJECT_ID)
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 firebase.analytics();
